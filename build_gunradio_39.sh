c2dd276867a9a53a2a5be01611328fb07c8c1de1
tomi <tomi@inspiron5491_preDJ.local>
2025-05-28 17:15:03 +0900

    first commit. added docker-gnuradio submodule.

---
+++ build_gunradio_39.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t ubuntu:gnuradio-releases-3.9 ./docker-gnuradio/gnuradio-releases-39
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# gnss_ws
+
+## 20250528
+
+- added docker gnuradio for latest ubuntu. (submodule + shell script).
